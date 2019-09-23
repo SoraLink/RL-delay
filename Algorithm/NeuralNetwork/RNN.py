2548e5e490dc537d8f20ea01816d1d4c1d7b406f
sora <qitianye1104@gmail.com>
2019-09-22 20:19:33 -0400

    env finish

--- Algorithm/NeuralNetwork/RNN.py
+++ Algorithm/NeuralNetwork/RNN.py
@@ -1,10 +1,12 @@
 import tensorflow as tf
 import numpy as np
 
+
 class RNN():
-    def __init__(self, sess, scope, hidden_units, max_step_length, input,
-                 make_value = 0.37):
-        self.input = input
+    def __init__(self, sess, scope, hidden_units, max_step_length, data_input, initial_states,
+                 make_value=0.37):
+        self.initial_state = initial_states
+        self.input = data_input
         self.sess = sess
         self.scope = scope
         self.hidden_units = hidden_units
@@ -16,12 +18,12 @@ class RNN():
         with tf.variable_scope(self.scope):
             out = tf.keras.layers.Masking(mask_value=self.mask_value)(self.input)
             for i, hidden in enumerate(self.hidden_units):
-                out = tf.keras.layers.GRU(hidden,return_sequences=True,return_state=True,
-                                              kernel_initializer='ones',recurrent_initializer='zeros',
-                                              use_bias=True,bias_initializer='ones')(out)
+                out = tf.keras.layers.GRU(hidden, return_sequences=True, return_state=True,
+                                          kernel_initializer='ones', recurrent_initializer='zeros',
+                                          use_bias=True, bias_initializer='ones') \
+                    (out, initial_state=self.initial_state[i])
         return out
 
-    def pad_input(self,data):
+    def pad_input(self, data):
         data = tf.keras.preprocessing.sequence.pad_sequences(data, maxlen=self.max_step_length)
         return data
-
