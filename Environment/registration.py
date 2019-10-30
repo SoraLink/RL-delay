3191f431197d8506a729aac0f9b67997a1ced313
TianyeQi <Tianye.Qi@mountsinai.org>
2019-10-30 16:45:13 -0400

    finish model and env

--- Environment/registration.py
+++ Environment/registration.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 
 class EnvRegistry(threading.Thread):
 
-    def __init__(self, task, transmit_delay=20, receive_delay=20, num_of_episode = 1000):
+    def __init__(self, task, transmit_delay=20, receive_delay=20, num_of_episode=1000):
         threading.Thread.__init__(self)
         self.env = gym.make(task)
         self.action_space = self.env.action_space
@@ -24,7 +24,7 @@ class EnvRegistry(threading.Thread):
         for i_episode in range(self.num_of_episode):
             self.env.reset()
             while True:
-                self.env.render()
+                # self.env.render()
                 if self.if_stop:
                     break
                 # self.observation, self.reward, self.done, self.info = self.env.step(self.action)
