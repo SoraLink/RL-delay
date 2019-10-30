118998a45b0fe5a6f475c7a41c8af9654e9f6431
TianyeQi <Tianye.Qi@mountsinai.org>
2019-10-30 16:35:30 -0400

    finish model and env

--- envTest.py
+++ envTest.py
@@ -5,12 +5,16 @@ def test():
     env.start()
     while True:
         action = env.action_space.sample()
-        observation, reward, done, info, action = env.step(action)
-        print("Observation: ", observation)
-        print("reward: ", reward)
-        print("done: ", done)
-        print("info: ", info)
-        print("action: ", action)
+        feedback = env.step(action)
+        if feedback is None:
+            print("Delaying...")
+        else:
+            observation, reward, done, info, action = feedback
+            print("Observation: ", observation)
+            print("reward: ", reward)
+            print("done: ", done)
+            print("info: ", info)
+            print("action: ", action)
 
 
 
